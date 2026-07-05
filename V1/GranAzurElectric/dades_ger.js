7cd956fdea67e06c2039753647ba18ea5c0ff486
Francesc Callejas <cesc@MacBook-Pro-de-Sistemes.local>
2026-07-05 13:24:50 +0200

    Nous models GranYosemite/GranAzurElectric, versions alemanyes, planols reals i logos

---
+++ V1/GranAzurElectric/dades_ger.js
@@ -0,0 +1,597 @@
+
+
+var dades={
+	highlights :
+	[
+		"4 SITZE / ISOFIX RÜCKSITZE",
+		"20/150L WASSERTANK",
+		"80 LITER ABFALLWASSERTANK/ ELEKTRISCHES ABFÜHR",
+		"THETFORD 90L KOMPRESSOR",
+		"GAS KOCHFELD, ELEKTRISCHE ZÜNDUNG",
+		"DOPPEL BETT",
+		"MOTORISIERTES HUBBETT VERSTELLBAR",
+		"THETFORD WC 400 SERIES",
+		"TRUMA COMBI 4D",
+		"MOSQUITONETZ MIT SCHIEBETÜR",
+		"MIDI HEKY SKY LIGHT 700X500",
+		"MAXFANN 400X400 SMOKE DE LUX",
+		"ALUMINIUMRAHMEN DUNKELFENSTER",
+		"ISOLIERTES BODEN",
+		"ISOLIERTES DACH",
+		"KAIFLEX ISOLIERUNG",
+		"ISOLIERTE KABINE",
+		"QUALITÄTSPOLSTER",
+		"SOLARZELLE",
+		"LITHIUM BEREIT ELEKTRISCHE KOMPONENTEN",
+		"ELEKTRISCHES BELEUCHTETE SCHIEBE TÜR",
+		"2 QUALITÄTSSTÜHLE AUF DEN HINTEREN TÜREN",
+		"LÄNGE 6.97 M",
+		"4250 kgs zGG",
+		"150 kw/204 Cv",
+		"BATTERIE 113kWh",
+		"HECKANTRIEB"
+	],
+	charac :
+	{
+		camper : 
+		[
+			"4 SITZE / ISOFIX RÜCKSITZE",
+			"20/150L WASSERTANK",
+			"80 LITER ABFALLWASSERTANK/ ELEKTRISCHES ABFÜHR",
+			"THETFORD 90L KOMPRESSOR",
+			"GAS KOCHFELD, ELEKTRISCHE ZÜNDUNG",
+			"DOPPEL BETT",
+			"MOTORISIERTES HUBBETT VERSTELLBAR",
+			"THETFORD WC 400 SERIES",
+			"TRUMA COMBI 4D",
+			"MOSQUITONETZ MIT SCHIEBETÜR",
+			"MIDI HEKY SKY LIGHT 700X500",
+			"MAXFANN 400X400 SMOKE DE LUX",
+			"ALUMINIUMRAHMEN DUNKELFENSTER",
+			"ISOLIERTES BODEN",
+			"ISOLIERTES DACH",
+			"KAIFLEX ISOLIERUNG",
+			"ISOLIERTE KABINE",
+			"QUALITÄTSPOLSTER",
+			"SOLARZELLE",
+			"LITHIUM READY ELECTRICAL COMPONENTS",
+			"ELEKTRISCHES BELEUCHTETE SCHIEBE TÜR",
+			"2 QUALITÄTSSTÜHLE AUF DEN HINTEREN TÜREN",
+			"LENGHT 6.97 M",
+			"4250 kgs zGG",
+			"150 kw/204 Cv",
+			"BATTERIE 113kWh",
+			"HECKANTRIEB"
+		],
+		chasis : [
+			{
+				code: "SK0/SK1",
+				desc: "HEAD SEAT COMFORT"
+			},
+			{
+				code : "B25",
+				desc: "ELECTRICAL PARKING BRAKE"
+			},
+			{
+				code: "R020",
+				desc:"SPARE WHEEL REMOVAL"
+			},
+			{
+				code: "R040",
+				desc:"SPARE WHEEL SUPPORT REMOVAL"
+			},
+			{
+				code: "XL2",
+				desc:"WHEIGHT VARIANT 4.100 kg"
+			},
+			{
+				code: "F64",
+				desc:"ELECTRICALLY FOLDABLE EXTERIOR MIRRORS"
+			},
+			{
+				code: "F68",
+				desc:"EXTERIOR MIRROR HEATED AND ELECTR. ADJUSTABLE"
+			},
+			{
+				code: "LG7",
+				desc:"LED HIGH PERFORMANCE HEADLAMPS"
+			},
+			{
+				code: "LB1",
+				desc:"SIDE MARKER LIGHTS"
+			},
+			{
+				code: "T74",
+				desc:"HANDLE AT ENTRANCE"
+			},
+			{
+				code: "T070",
+				desc:"SUPRESION DE T86"
+			},
+			{
+				code: "H15",
+				desc:"SEAT HEATING FOR CO-DRIVER"
+			},					
+			{
+				code: "H16",
+				desc:"HEATED SEAT FOR DRIVER"
+			},
+			{
+				code: "SB1",
+				desc:"COMFORT DRIVER'S SEAT"
+			},
+			{
+				code: "SB2",
+				desc:"COMFORT CO-DRIVER'S SEAT"
+			},
+			{
+				code: "S25",
+				desc:"ARMREST FOR CO-DRIVER'S SEAT"
+			},
+			{
+				code: "S88",
+				desc:"CO-DRIVER'S SEAT FRAME, LOW"
+			},
+			{
+				code: "SR8",
+				desc:"SWIVEL BASE FOR DRIVER' SEAT"
+			},
+			{
+				code: "SR9",
+				desc:"SWIVEL BASE FOR CO-DRIVER' SEAT"
+			},
+			{
+				code: "S22",
+				desc:"ARMREST FOR DRIVER'S SEAT"
+			},
+			{
+				code: "S28",
+				desc:"ARMREST, DRIVER'S AND CO-DRIVER'S DOOR"
+			},
+			{
+				code: "SZ8",
+				desc:"PASSENGER BACKREST STORAGE NET"
+			},
+			{
+				code: "SZ7",
+				desc:"DRIVER BACKREST STORAGE NET"
+			},
+			{
+				code: "SA6",
+				desc:"AIRBAG, CO-DRIVER"
+			},
+			{
+				code: "FJ5",
+				desc:"HINGED LID FOR STOWAGE COMPARTMENTS LEFT AND RIGHT"
+			},
+			{
+				code: "FJ1",
+				desc:"HINGED LID FOR STOWAGE COMPARTMENT"
+			},
+			{
+				code: "FQ6",
+				desc:"LOCKABLE UNIT IN STOW. COMPARTMENT OVER WINDSCREEN"
+			},
+			{
+				code: "FG8",
+				desc:"FRONT CUP HOLDER"
+			},
+			{
+				code: "V94",
+				desc:"CONDUCTION CHANNEL IN THE SIDE WALL"
+			},
+			{
+				code: "V95",
+				desc:"CONDUCTION CHANNEL IN THE REAR PORTAL"
+			},
+			{
+				code: "V93",
+				desc:"OMISSION WOODEN FLOOR"
+			},
+			{
+				code: "CL3",
+				desc:"LEATHER STEERING WHEEL"
+			},
+			{
+				code: "XM4",
+				desc:"ACOUSTIC PACKAGE"
+			},
+			{
+				code: "KL1",
+				desc:"INDEPENDENT HEATING TANK TRANSMITTER"
+			},
+			{
+				code: "HH4",
+				desc:"THERMOTRONIC AUTOMATIC CLIMATE CONTROL"
+			},
+			{
+				code: "XO5",
+				desc:"DIGITAL OWNER'S MANUAL"
+			},
+			{
+				code: "EX9",
+				desc:"FREE UPDATE OF CARTOGRAPHIC DATA FOR 3 YEARS"
+			},
+			{
+				code: "E4M",
+				desc:"MBUX MULTIMEDIA SYSTEM WITH 10.25-INCH TOUCHSCREEN"
+			},
+			{
+				code: "E5M",
+				desc:"EXTENSION OF PSM STANDARD CONTACT"
+			},
+			{
+				code: "EK1",
+				desc:"TERMINAL STRIP FOR ELECTRICAL CONNECTION"
+			},
+			{
+				code: "JW8",
+				desc:"ATTENTION ASSIST"
+			},
+			{
+				code: "C6L",
+				desc:"MULTIFUNCTION STEERING WHEEL"
+			},
+			{
+				code: "CL1",
+				desc:"HEIGHT AND RAKE ADJUSTABLE STEERING WHEEL"
+			},
+			{
+				code: "E3J",
+				desc:"SWITCH PANEL PRE-EQUIPMENT"
+			},
+			{
+				code: "JW4",
+				desc:"BELT NOT FASTENED WARNING DESACTIVATION"
+			},
+			{
+				code: "JK5",
+				desc:"INSTRUMENT CLUSTER WITH COLOUR DISPLAY"
+			},
+			{
+				code: "JB6",
+				desc:"CAMERA PARKING PACKAGE 360g"
+			},
+			{
+				code: "ET4",
+				desc:"ACTIVE DISTANCE ASSIST DISTRONIC"
+			},
+			{
+				code: "M94",
+				desc:"VEHICLE WITHOUT ROAD SPEED LIMITER EG"
+			},
+			{
+				code: "Y26",
+				desc:"CALCE"
+			},
+			{
+				code: "54",
+				desc:"AÑO MOD. X3/2"
+			},
+			{
+				code: "EY2",
+				desc:"PRE-INSTALLATION: LIVE TRAFFIC INFORMATION"
+			},
+			{
+				code: "FM3",
+				desc:"WET WIPER SYSTEM"
+			},
+			{
+				code: "E1D",
+				desc:"DIGITAL RADIO (DAB)"
+			},
+			{
+				code: "ED4",
+				desc:"HEAVY-DUTY BATTERY"
+			},
+			{
+				code: "M40",
+				desc:"ALTERNATOR 14 V / 200 A"
+			},
+			{
+				code: "ES0",
+				desc:"STARTING AID CONTACT"
+			},
+			{
+				code: "LA2",
+				desc:"HEADLIGHT ASSISTANT"
+			},
+			{
+				code: "JW5",
+				desc:"LANE KEEPING ASSIST"
+			},
+			{
+				code: "JH3",
+				desc:"COMMUNICATION MODULE (LTE) FOR DIGITAL SERVICES"
+			},
+			{
+				code: "H1B",
+				desc:"TACHOGRAPH OPERATION AT THE FRONT UNDER THE INTERIOR ROOF"
+			},
+			{
+				code: "JF1",
+				desc:"RAIN SENSOR"
+			},
+			{
+				code: "J58",
+				desc:"SEAT BELT WARNING FOR DRIVER'S SEAT"
+			},
+			{
+				code: "J65",
+				desc:"TEMPERATURE INDICATOR"
+			},
+			{
+				code: "JA8",
+				desc:"CROSSWIND ASSIST"
+			},
+			{
+				code: "J10",
+				desc:"SPEEDOMETER, KM/H"
+			},
+			{
+				code: "JD6",
+				desc:"SMART TACHOGRAPH UE"
+			},
+			{
+				code: "J55",
+				desc:"SEAT BELT REMINDER FOR CO-DRIVER SEAT"
+			},
+			{
+				code: "ED8",
+				desc:"PREINSTALLATIONS ELECTRICAL SYSTEM, PSM"
+			},
+			{
+				code: "E07",
+				desc:"BREAKDOWN MANAGEMENT"
+			},
+			{
+				code: "MU6",
+				desc:"ENGINE OM 654 DE 20 LA 140 KW (190 HP)"
+			},
+			{
+				code: "MP6",
+				desc:"ENGINE VERSION EURO VI"
+			},
+			{
+				code: "MJ8",
+				desc:"ECO START/STOP FUNCTION"
+			},
+			{
+				code: "MR0",
+				desc:"EMISSIONS STANDARD E (HEAVY-DUTY COMM. VEHICLES)"
+			},
+			{
+				code: "G43",
+				desc:"9G-TRONIC"
+			},
+			{
+				code: "A4M",
+				desc:"4X4 ALL-WHEEL DRIVE WITH TORQUE-ON-DEMAND"
+			},
+			{	
+				code: "KB7",
+				desc:"MAIN TANK, 93 LITRES"
+			},
+			{
+				code: "M72",
+				desc:"VEHICLE HVO-CAPABLE"
+			},
+			{
+				code: "D03",
+				desc:"HIGH ROOF"
+			},
+			{
+				code: "FU5",
+				desc:"EXTERIOR MIRRORS WITHOUT INDICATORS"
+			},
+			{
+				code: "Q11",
+				desc:"LONGITUDINAL MEMBER REINFORCEMENT"
+			},
+			{
+				code: "LE1",
+				desc:"ADAPTIVE BRAKE LIGHT"
+			},
+			{
+				code: "L13",
+				desc:"FOG LAMPS WITH CORNERING LIGHT FUNCTION"
+			},
+			{
+				code: "LC4",
+				desc:"COMFORT OVERHEAD CONTROL PANEL"
+			},
+			{
+				code: "LB5",
+				desc:"3RD BRAKE LIGHT"
+			},
+			{
+				code: "L44",
+				desc:"FLASHING LIGHTS, FRONT LEFT"
+			},
+			{
+				code: "L22",
+				desc:"PARTIAL LED TAIL LIGHTS"
+			},
+			{
+				code: "T16",
+				desc:"SLIDING DOOR RIGHT"
+			},
+			{
+				code: "T75",
+				desc:"GRAB HANDLES FOR DRIVER'S AND CO-DRIVER'S DOORS"
+			},
+			{
+				code: "H21",
+				desc:"HEAT-INSULATING GLASS W.BAND FILTER ON WINDSCREEN"
+			},
+			{
+				code: "CN2",
+				desc:"RADIATOR GRILLE FRAME IN VEHICLE COLOUR"
+			},
+			{
+				code: "EY5",
+				desc:"MERCEDES-BENZ EMERGENCY CALL SYSTEM"
+			},
+			{
+				code: "EY6",
+				desc:"BREAKDOWN MANAGEMENT"
+			},
+			{
+				code: "EW6",
+				desc:"PRE-INSTALLATION: REMOTE SERVICES PLUS"
+			},
+			{
+				code: "Z74",
+				desc:"BODYSHELL, ADDITIONAL MEASURES 2"
+			},
+			{
+				code: "Z75",
+				desc:"BODYSHELL, ADDITIONAL MEASURES"
+			},
+			{
+				code: "Z4V",
+				desc:"PRODUCTION IN DÜSSELDORF"
+			},
+			{
+				code: "XZ0",
+				desc:"MODEL GENERATION 0"
+			},
+			{
+				code: "804",
+				desc:"CY X3/1"
+			},
+			{
+				code: "SA5",
+				desc:"AIRBAG, FRONT DRIVER'S SEAT"
+			},
+			{
+				code: "FJ4",
+				desc:"STOWAGE COMPARTMENT UNDERNEATH COCKPIT"
+			},
+			{
+				code: "FF5",
+				desc:"SHELF ABOVE WINDSCREEN"
+			},
+			{
+				code: "V85",
+				desc:"SMOKER PACKAGE"
+			},
+			{
+				code: "IG4",
+				desc:"STANDARD"
+			},
+			{
+				code: "IG5",
+				desc:"BASIC"
+			},
+			{
+				code: "AR2",
+				desc:"AXLE RATIO I = 3,923"
+			},
+			{
+				code: "BA3",
+				desc:"ACTIVE BRAKE ASSIST"
+			},
+			{
+				code: "RG8",
+				desc:"TYRES 225/75 R16 C"
+			},
+			{
+				code: "RM7",
+				desc:"SUMMER TYRES"
+			},
+			{
+				code: "BH1",
+				desc:"HOLD FUNCTION"
+			}
+		]
+	},
+	price:{
+		base: [
+			{
+				concept: "GRUNDPREIS GRAN AZUR E SPRINTER",
+				price: 122989.00
+			},
+			{
+				concept: "HOCHWERTIGE LEDERPOLSTERUNG MIT STREIFEN",
+				price: 2750.00
+			},
+			{
+				separa: true,
+				concept: "NETTOPREIS",
+				price: 125739.00 
+			},
+			{
+				concept: "UMSATZSTEUER 19% (Mwst)",
+				price: 23890.41
+			},
+			{
+				separa: true,
+				total: true,
+				concept: "ENDPREIS ZUZUEGLICH TRANSPORTKOSTEN",
+				price: 149629.41 
+			}
+		],
+		expo: [
+			{
+				concept: "GRUNDPREIS GRAN AZUR E SPRINTER",
+				price: 122989.00
+			},
+			{			
+				concept: "AUFSTELLDACH W907 L (4.1t)",
+				price: 7010.00
+			},
+			{
+				concept: "HOCHWERTIGE LEDERPOLSTERUNG MIT STREIFEN",
+				price: 2750.00
+			},
+			{			
+				concept: "AUSSENDUSCHE",
+				price: 240.00
+			},
+			{			
+				concept: "INDUKTIONSKOCHFELDR",
+				price: 1800.00
+			},
+			{			
+				concept: "WC CLESANA C1",
+				price: 1900.00
+			},
+			{
+				concept: "AT REIFEN 18ZOLL",
+				price: 1275.00
+			},
+			{
+				concept: "ALU RAEDER CLASSIC B 18x9+20 CB84,1 6X130 SCHWARZ MATT",
+				price: 3550.00
+			},
+			{
+				concept: "RADHAUSVERBREITERUNG MER SPRINTER 6P",
+				price: 1875.00
+			},
+			{
+				concept: "ARBEITSLEUCHTEN LAZER LIGHT MAXX",
+				price: 450.00
+			},
+			{
+				concept: "DACHTRAEGER",
+				price: 1350.00
+			},
+			{			
+				separa : true,
+				concept: "NETTOPREIS",
+				price: 143839.00
+			},
+			{
+				concept: "UMSATZSTEUER 19% (Mwst)",
+				price: 27329.41
+			},
+			{
+				separa : true,
+				total: true,
+				concept: "ENDPREIS ZUZUEGLICH TRANSPORTKOSTEN",
+				price: 171168.41
+			}
+		]
+	}
+};
---
+++ V1/GranYosemite4x4/dades_ger.js
@@ -0,0 +1,605 @@
+
+
+var dades={
+	highlights :
+	[
+		"4 SITZE / ISOFIX RÜCKSITZE",
+		"20/150L WASSERTANK",
+		"80 LITER ABFALLWASSERTANK/ ELEKTRISCHES ABFÜHR",
+		"THETFORD 90L KOMPRESSOR",
+		"GAS KOCHFELD, ELEKTRISCHE ZÜNDUNG",
+		"DOPPEL BETT",
+		"MOTORISIERTES HUBBETT VERSTELLBAR",
+		"UNTEREN BETT MIT ABSTELLMÖGLICHKEIT",
+		"THETFORD WC 400 SERIES",
+		"TRUMA COMBI 4D",
+		"MOSQUITONETZ MIT SCHIEBETÜR",
+		"",
+		"MIDI HEKY SKY LIGHT 700X500",
+		"MAXFANN 400X400 SMOKE DE LUX",
+		"ALUMINIUMRAHMEN DUNKELFENSTER",
+		"ISOLIERTES BODEN",
+		"ISOLIERTES DACH",
+		"KAIFLEX ISOLIERUNG",
+		"ISOLIERTE KABINE",
+		"QUALITÄTSPOLSTER",
+		"",
+		"SOLARZELLE",
+		"LITHIUM READY ELECTRICAL COMPONENTS",
+		"ELEKTRISCHES BELEUCHTETE SCHIEBE TÜR",
+		"",
+		"2 QUALITÄTSSTÜHLE AUF DEN HINTEREN TÜREN",
+		"6.97M"
+	],
+	charac :
+	{
+		camper : 
+		[
+			"4 SITZE / ISOFIX RÜCKSITZE",
+			"20/150 LITER FRISCHES WASSERTANK",
+			"80 LITER ABFALLWASSERTANK/ ELEKTRISCHES ABFÜHR",
+			"THETFORD 90L KOMPRESSOR",
+			"GAS KOCHFELD, ELEKTRISCHE ZÜNDUNG",
+			"DOPPEL BETT",
+			"MOTORISIERTES HUBBETT VERSTELLBAR",
+			"UNTEREN BETT MIT ABSTELLMÖGLICHKEIT",
+			"THETFORD WC 400 SERIES",
+			"TRUMA COMBI 4D",
+			"MOSQUITONETZ MIT SCHIEBETÜR",
+			"",
+			"MIDI HEKY SKY LIGHT 700X500",
+			"MAXFANN 400X400 SMOKE DE LUX",
+			"ALUMINIUMRAHMEN DUNKELFENSTER",
+			"ISOLIERTES BODEN",
+			"ISOLIERTES DACH",
+			"KAIFLEX ISOLIERUNG",
+			"ISOLIERTE KABINE",
+			"QUALITÄTSPOLSTER",
+			"",
+			"SOLARZELLE",
+			"LITHIUM READY ELECTRICAL COMPONENTS",
+			"ELEKTRISCHES BELEUCHTETE SCHIEBE TÜR",
+			"",
+			"2 QUALITÄTSSTÜHLE AUF DEN HINTEREN TÜREN",
+			"6.97M"
+		],
+		chasis : [
+			{
+				code: "SK0/SK1",
+				desc: "HEAD SEAT COMFORT"
+			},
+			{
+				code : "B25",
+				desc: "ELECTRICAL PARKING BRAKE"
+			},
+			{
+				code: "R020",
+				desc:"SPARE WHEEL REMOVAL"
+			},
+			{
+				code: "R040",
+				desc:"SPARE WHEEL SUPPORT REMOVAL"
+			},
+			{
+				code: "XL2",
+				desc:"WHEIGHT VARIANT 4.100 kg"
+			},
+			{
+				code: "F64",
+				desc:"ELECTRICALLY FOLDABLE EXTERIOR MIRRORS"
+			},
+			{
+				code: "F68",
+				desc:"EXTERIOR MIRROR HEATED AND ELECTR. ADJUSTABLE"
+			},
+			{
+				code: "LG7",
+				desc:"LED HIGH PERFORMANCE HEADLAMPS"
+			},
+			{
+				code: "LB1",
+				desc:"SIDE MARKER LIGHTS"
+			},
+			{
+				code: "T74",
+				desc:"HANDLE AT ENTRANCE"
+			},
+			{
+				code: "T070",
+				desc:"SUPRESION DE T86"
+			},
+			{
+				code: "H15",
+				desc:"SEAT HEATING FOR CO-DRIVER"
+			},					
+			{
+				code: "H16",
+				desc:"HEATED SEAT FOR DRIVER"
+			},
+			{
+				code: "SB1",
+				desc:"COMFORT DRIVER'S SEAT"
+			},
+			{
+				code: "SB2",
+				desc:"COMFORT CO-DRIVER'S SEAT"
+			},
+			{
+				code: "S25",
+				desc:"ARMREST FOR CO-DRIVER'S SEAT"
+			},
+			{
+				code: "S88",
+				desc:"CO-DRIVER'S SEAT FRAME, LOW"
+			},
+			{
+				code: "SR8",
+				desc:"SWIVEL BASE FOR DRIVER' SEAT"
+			},
+			{
+				code: "SR9",
+				desc:"SWIVEL BASE FOR CO-DRIVER' SEAT"
+			},
+			{
+				code: "S22",
+				desc:"ARMREST FOR DRIVER'S SEAT"
+			},
+			{
+				code: "S28",
+				desc:"ARMREST, DRIVER'S AND CO-DRIVER'S DOOR"
+			},
+			{
+				code: "SZ8",
+				desc:"PASSENGER BACKREST STORAGE NET"
+			},
+			{
+				code: "SZ7",
+				desc:"DRIVER BACKREST STORAGE NET"
+			},
+			{
+				code: "SA6",
+				desc:"AIRBAG, CO-DRIVER"
+			},
+			{
+				code: "FJ5",
+				desc:"HINGED LID FOR STOWAGE COMPARTMENTS LEFT AND RIGHT"
+			},
+			{
+				code: "FJ1",
+				desc:"HINGED LID FOR STOWAGE COMPARTMENT"
+			},
+			{
+				code: "FQ6",
+				desc:"LOCKABLE UNIT IN STOW. COMPARTMENT OVER WINDSCREEN"
+			},
+			{
+				code: "FG8",
+				desc:"FRONT CUP HOLDER"
+			},
+			{
+				code: "V94",
+				desc:"CONDUCTION CHANNEL IN THE SIDE WALL"
+			},
+			{
+				code: "V95",
+				desc:"CONDUCTION CHANNEL IN THE REAR PORTAL"
+			},
+			{
+				code: "V93",
+				desc:"OMISSION WOODEN FLOOR"
+			},
+			{
+				code: "CL3",
+				desc:"LEATHER STEERING WHEEL"
+			},
+			{
+				code: "XM4",
+				desc:"ACOUSTIC PACKAGE"
+			},
+			{
+				code: "KL1",
+				desc:"INDEPENDENT HEATING TANK TRANSMITTER"
+			},
+			{
+				code: "HH4",
+				desc:"THERMOTRONIC AUTOMATIC CLIMATE CONTROL"
+			},
+			{
+				code: "XO5",
+				desc:"DIGITAL OWNER'S MANUAL"
+			},
+			{
+				code: "EX9",
+				desc:"FREE UPDATE OF CARTOGRAPHIC DATA FOR 3 YEARS"
+			},
+			{
+				code: "E4M",
+				desc:"MBUX MULTIMEDIA SYSTEM WITH 10.25-INCH TOUCHSCREEN"
+			},
+			{
+				code: "E5M",
+				desc:"EXTENSION OF PSM STANDARD CONTACT"
+			},
+			{
+				code: "EK1",
+				desc:"TERMINAL STRIP FOR ELECTRICAL CONNECTION"
+			},
+			{
+				code: "JW8",
+				desc:"ATTENTION ASSIST"
+			},
+			{
+				code: "C6L",
+				desc:"MULTIFUNCTION STEERING WHEEL"
+			},
+			{
+				code: "CL1",
+				desc:"HEIGHT AND RAKE ADJUSTABLE STEERING WHEEL"
+			},
+			{
+				code: "E3J",
+				desc:"SWITCH PANEL PRE-EQUIPMENT"
+			},
+			{
+				code: "JW4",
+				desc:"BELT NOT FASTENED WARNING DESACTIVATION"
+			},
+			{
+				code: "JK5",
+				desc:"INSTRUMENT CLUSTER WITH COLOUR DISPLAY"
+			},
+			{
+				code: "JB6",
+				desc:"CAMERA PARKING PACKAGE 360g"
+			},
+			{
+				code: "ET4",
+				desc:"ACTIVE DISTANCE ASSIST DISTRONIC"
+			},
+			{
+				code: "M94",
+				desc:"VEHICLE WITHOUT ROAD SPEED LIMITER EG"
+			},
+			{
+				code: "Y26",
+				desc:"CALCE"
+			},
+			{
+				code: "54",
+				desc:"AÑO MOD. X3/2"
+			},
+			{
+				code: "EY2",
+				desc:"PRE-INSTALLATION: LIVE TRAFFIC INFORMATION"
+			},
+			{
+				code: "FM3",
+				desc:"WET WIPER SYSTEM"
+			},
+			{
+				code: "E1D",
+				desc:"DIGITAL RADIO (DAB)"
+			},
+			{
+				code: "ED4",
+				desc:"HEAVY-DUTY BATTERY"
+			},
+			{
+				code: "M40",
+				desc:"ALTERNATOR 14 V / 200 A"
+			},
+			{
+				code: "ES0",
+				desc:"STARTING AID CONTACT"
+			},
+			{
+				code: "LA2",
+				desc:"HEADLIGHT ASSISTANT"
+			},
+			{
+				code: "JW5",
+				desc:"LANE KEEPING ASSIST"
+			},
+			{
+				code: "JH3",
+				desc:"COMMUNICATION MODULE (LTE) FOR DIGITAL SERVICES"
+			},
+			{
+				code: "H1B",
+				desc:"TACHOGRAPH OPERATION AT THE FRONT UNDER THE INTERIOR ROOF"
+			},
+			{
+				code: "JF1",
+				desc:"RAIN SENSOR"
+			},
+			{
+				code: "J58",
+				desc:"SEAT BELT WARNING FOR DRIVER'S SEAT"
+			},
+			{
+				code: "J65",
+				desc:"TEMPERATURE INDICATOR"
+			},
+			{
+				code: "JA8",
+				desc:"CROSSWIND ASSIST"
+			},
+			{
+				code: "J10",
+				desc:"SPEEDOMETER, KM/H"
+			},
+			{
+				code: "JD6",
+				desc:"SMART TACHOGRAPH UE"
+			},
+			{
+				code: "J55",
+				desc:"SEAT BELT REMINDER FOR CO-DRIVER SEAT"
+			},
+			{
+				code: "ED8",
+				desc:"PREINSTALLATIONS ELECTRICAL SYSTEM, PSM"
+			},
+			{
+				code: "E07",
+				desc:"BREAKDOWN MANAGEMENT"
+			},
+			{
+				code: "MU6",
+				desc:"ENGINE OM 654 DE 20 LA 140 KW (190 HP)"
+			},
+			{
+				code: "MP6",
+				desc:"ENGINE VERSION EURO VI"
+			},
+			{
+				code: "MJ8",
+				desc:"ECO START/STOP FUNCTION"
+			},
+			{
+				code: "MR0",
+				desc:"EMISSIONS STANDARD E (HEAVY-DUTY COMM. VEHICLES)"
+			},
+			{
+				code: "G43",
+				desc:"9G-TRONIC"
+			},
+			{
+				code: "A4M",
+				desc:"4X4 ALL-WHEEL DRIVE WITH TORQUE-ON-DEMAND"
+			},
+			{	
+				code: "KB7",
+				desc:"MAIN TANK, 93 LITRES"
+			},
+			{
+				code: "M72",
+				desc:"VEHICLE HVO-CAPABLE"
+			},
+			{
+				code: "D03",
+				desc:"HIGH ROOF"
+			},
+			{
+				code: "FU5",
+				desc:"EXTERIOR MIRRORS WITHOUT INDICATORS"
+			},
+			{
+				code: "Q11",
+				desc:"LONGITUDINAL MEMBER REINFORCEMENT"
+			},
+			{
+				code: "LE1",
+				desc:"ADAPTIVE BRAKE LIGHT"
+			},
+			{
+				code: "L13",
+				desc:"FOG LAMPS WITH CORNERING LIGHT FUNCTION"
+			},
+			{
+				code: "LC4",
+				desc:"COMFORT OVERHEAD CONTROL PANEL"
+			},
+			{
+				code: "LB5",
+				desc:"3RD BRAKE LIGHT"
+			},
+			{
+				code: "L44",
+				desc:"FLASHING LIGHTS, FRONT LEFT"
+			},
+			{
+				code: "L22",
+				desc:"PARTIAL LED TAIL LIGHTS"
+			},
+			{
+				code: "T16",
+				desc:"SLIDING DOOR RIGHT"
+			},
+			{
+				code: "T75",
+				desc:"GRAB HANDLES FOR DRIVER'S AND CO-DRIVER'S DOORS"
+			},
+			{
+				code: "H21",
+				desc:"HEAT-INSULATING GLASS W.BAND FILTER ON WINDSCREEN"
+			},
+			{
+				code: "CN2",
+				desc:"RADIATOR GRILLE FRAME IN VEHICLE COLOUR"
+			},
+			{
+				code: "EY5",
+				desc:"MERCEDES-BENZ EMERGENCY CALL SYSTEM"
+			},
+			{
+				code: "EY6",
+				desc:"BREAKDOWN MANAGEMENT"
+			},
+			{
+				code: "EW6",
+				desc:"PRE-INSTALLATION: REMOTE SERVICES PLUS"
+			},
+			{
+				code: "Z74",
+				desc:"BODYSHELL, ADDITIONAL MEASURES 2"
+			},
+			{
+				code: "Z75",
+				desc:"BODYSHELL, ADDITIONAL MEASURES"
+			},
+			{
+				code: "Z4V",
+				desc:"PRODUCTION IN DÜSSELDORF"
+			},
+			{
+				code: "XZ0",
+				desc:"MODEL GENERATION 0"
+			},
+			{
+				code: "804",
+				desc:"CY X3/1"
+			},
+			{
+				code: "SA5",
+				desc:"AIRBAG, FRONT DRIVER'S SEAT"
+			},
+			{
+				code: "FJ4",
+				desc:"STOWAGE COMPARTMENT UNDERNEATH COCKPIT"
+			},
+			{
+				code: "FF5",
+				desc:"SHELF ABOVE WINDSCREEN"
+			},
+			{
+				code: "V85",
+				desc:"SMOKER PACKAGE"
+			},
+			{
+				code: "IG4",
+				desc:"STANDARD"
+			},
+			{
+				code: "IG5",
+				desc:"BASIC"
+			},
+			{
+				code: "AR2",
+				desc:"AXLE RATIO I = 3,923"
+			},
+			{
+				code: "BA3",
+				desc:"ACTIVE BRAKE ASSIST"
+			},
+			{
+				code: "RG8",
+				desc:"TYRES 225/75 R16 C"
+			},
+			{
+				code: "RM7",
+				desc:"SUMMER TYRES"
+			},
+			{
+				code: "BH1",
+				desc:"HOLD FUNCTION"
+			}
+		]
+	},
+	price:{
+		base: [
+			{
+				concept: "GRUNDPREIS GRAN YOSEMITE 419 4X4",
+				price: 112450.00
+			},
+			{
+				separa: true,
+				concept: "NETTOPREIS",
+				price: 112450.00 
+			},
+			{
+				concept: "UMSATZSTEUER 19% (Mwst)",
+				price: 21365.50
+			},
+			{
+				separa: true,
+				total: true,
+				concept: "ENDPREIS ZUZUEGLICH TRANSPORTKOSTEN",
+				price: 133815.50 
+			}
+		],
+		expo: [
+			{
+				concept: "GRUNDPREIS GRAN YOSEMITE 419 4X4",
+				price: 112450.00
+			},
+			{
+				total: true,
+				concept: "FAHRGESTELLKONFIGURATION"
+			},
+			{
+				concept: "METALLIC TERNORITE 7755 LACKIERUNG",
+				price: 2180.00
+			},			
+			{
+				total: true,
+				concept: "CAMPER CONFIGURATION"
+			},
+			{
+				concept: "AUFSTELLDACH W907 M",
+				price: 7010.00
+			},
+			{
+				concept: "HOCHWERTIGE LEDERPOLSTERUNG MIT DIAMANTEN",
+				price: 2750.00
+			},
+			{
+				concept: "WC CLESANA C1",
+				price: 1050.00
+			},
+			{
+				concept: "GOODRICH 265/60/18 (4/5) REIFEN",
+				price: 1275.00
+			},
+			{
+				concept: "ALU RAEDER CLASSIC B 18x9+45 CB84,1 6X130 SCHWARTZ MATT",
+				price: 3150.00
+			},
+			{
+				concept: "LAZER SCHEINWERFER MER W907 TRIPLE R",
+				price: 930.00
+			},
+			{
+				concept: "KLIMAANLAGE (TELAIR E VAN 7400h)",
+				price: 3100.00
+			},
+			{
+				concept: "REMIS KABINE",
+				price: 850.00
+			},
+			{
+				concept: "THULE 400 ANTRACITE FÜR AUFSTELLDACH MODELLE",
+				price: 1600.00
+			},
+			{
+				concept: "HOHE HINTERE RADHALTERUNG ALU W907",
+				price: 1450.00
+			},
+			{
+				separa : true,
+				concept: "NETTOPREIS",
+				price: 137795.00
+			},
+			{
+				concept: "UMSATZSTEUER 19% (Mwst)",
+				price: 26181.05
+			},
+			{
+				separa : true,
+				total: true,
+				concept: "ENDPREIS ZUZUEGLICH TRANSPORTKOSTEN",
+				price: 163976.05
+			}
+		]
+	}
+};
